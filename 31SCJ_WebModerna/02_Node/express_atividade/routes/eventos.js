14a5200b51e5be8cd1462b0a2900d9eeb3b769e6
logonrm <logonrm@fiap.com.br>
2018-06-16 09:07:44 -0300

    Call middleware before controler action

--- 31SCJ_WebModerna/02_Node/express_atividade/routes/eventos.js
+++ 31SCJ_WebModerna/02_Node/express_atividade/routes/eventos.js
@@ -1,10 +1,10 @@
 module.exports = function (app) {
-    var valida = require('./../middlewares/valida'); 
-    var eventos = app.controllers.eventos;
-    app.get('/menu', eventos.menu);
-    app.get('/cadUsuario', eventos.cadastroUsuario);
-    app.get('/cadEvento', eventos.cadastroEvento); 
-    app.get('/listaEventos', eventos.listaEventos);
+    var valida = require('./../middlewares/valida')
 
-    app.post('/novoEvento', eventos.novoEvento);
-}; 
+    var eventos = app.controllers.eventos;
+    app.get('/menu', valida, eventos.menu);
+
+    app.get('/cadUsuario', valida, eventos.cadastroUsuario);
+    app.get('/cadEvento', valida,eventos.cadastroEvento);
+    app.get('/listaEventos', valida, eventos.listaEventos);
+}
